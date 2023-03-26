7fe7acf3b61db0547d7805b1dd0739fec7825b08
guibad22daw <guibad22daw@proton.me>
2023-03-26 03:34:27 +0200

    Homepage movil acabada

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
 import React, { useContext } from "react";
 import { FilterBar, PokemonList } from "../components";
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { PokemonContext } from "../context/PokemonContext";
 
 export const HomePage = () => {
-    const { onClickLoadMore, active, setActive } = useContext(PokemonContext)
+    const { onClickLoadMore, active, setActive, onInputChange, valueSearch, onResetForm } = useContext(PokemonContext);
+    const navigate = useNavigate();
+
+	const onSearchSubmit = e => {
+		e.preventDefault();
+		navigate('/search', {
+			state: valueSearch,
+		});
+
+		onResetForm();
+	};
     
     return (
         <>
@@ -25,23 +36,16 @@ export const HomePage = () => {
                     </svg>
                     <span>Filtrar</span>
                 </div>
-                <div className="icon-buscar" onClick={() => setActive(!active)}>
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        strokeWidth='1.5'
-                        stroke='currentColor'
-                        className='img-buscar'
-                    >
-                        <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
-                        />
-                    </svg>
-                    <span style={{marginLeft:"-6.5em"}}>Buscar</span>
-                </div>
+				<form onSubmit={onSearchSubmit} className="input-i-boton">
+					<div className='form-group'>
+						<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='icon-search'>
+							<path strokeLinecap='round'strokeLinejoin='round' d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'/>
+						</svg>
+						<input type='search' name='valueSearch' id='' value={valueSearch} onChange={onInputChange} placeholder='Introduce pokemon'/>
+					</div>
+
+					<button className='btn-search'>Buscar</button>
+				</form>
             </div>
             <PokemonList />
             <FilterBar />
